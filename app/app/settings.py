32dd8b455874c6e6e216a1f8c9a9a43d9ddb861f
qamiln <kamil.nowak@cubasles.pl>
2021-04-11 13:47:58 +0200

    fx

--- app/app/settings.py
+++ app/app/settings.py
@@ -73,15 +73,14 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME':  os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASS'),
+        'NAME': 'app',
+        'USER': 'docker',
+        'PASSWORD': 'docker',
         'HOST': os.environ.get('DB_HOST'),
-        'PORT': os.environ.get('DB_PORT'),
+        'PORT': '5432',
     }
 }
 
