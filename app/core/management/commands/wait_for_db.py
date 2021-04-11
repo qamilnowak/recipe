22c8bedf7c60642658e13ab58cb7a546f73a91aa
qamiln <kamil.nowak@cubasles.pl>
2021-04-11 10:55:45 +0200

    test

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -1,8 +1,6 @@
-import time
-from django.db import connections
+from django.db import connectitimeons
 from django.db.utils import OperationalError
 from django.core.management.base import BaseCommand
-
 
 class Command(BaseCommand):
     """Django command to pause execution until database is available"""
